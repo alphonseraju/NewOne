bb9378de7b8cf5939a011481f160bf199f472c61
alphonseraju <149765081+alphonseraju@users.noreply.github.com>
2023-12-30 09:50:07 +0530

    Add files via upload

---
+++ second.java
@@ -0,0 +1,31 @@
+package DaliyClassCodes;
+
+public class second {
+	public class Car {
+	    private String model;
+	    private int year;
+
+	    public Car(String model, int year) {
+	        this.model = model;
+	        this.year = year;
+	    }
+
+	    public String getModel() {
+	        return model;
+	    }
+
+	    public void setModel(String model) {
+	        this.model = model;
+	    }
+
+	    public int getYear() {
+	        return year;
+	    }
+
+	    public void setYear(int year) {
+	        this.year = year;
+	    }
+	}
+
+
+}
